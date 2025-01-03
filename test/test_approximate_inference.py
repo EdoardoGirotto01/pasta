86e482b125d8216275eb720e0e2a0b84dd61451a
Edoardo Girotto <edoardo@Air-di-Edoardo.homenet.telecomitalia.it>
2025-01-03 17:02:09 +0100

    Amplia i test di approximate_inference e exact_inference

--- test/test_approximate_inference.py
+++ test/test_approximate_inference.py
@@ -8,17 +8,28 @@ from .utils_for_tests import ArgumentsTest, almost_equal
 
 @pytest.mark.parametrize("parameters", [
     ArgumentsTest("alarm_calls_mary", "../examples/inference/alarm.lp", "calls(mary)", 0.196, 0.196),
-    ArgumentsTest("bird_10_fly_1","../examples/inference/bird_10.lp", "fly(1)", 0.126953, 0.5),
+    ArgumentsTest("barber_person_j", "../examples/inference/barber.lp", "person(j)", 1.0, 1.0),
     ArgumentsTest("bird_2_2_fly_1", "../examples/inference/bird_2_2.lp", "fly_1", 0.6, 0.7),
     ArgumentsTest("bird_4_fly_1", "../examples/inference/bird_4.lp", "fly(1)", 0.25, 0.5),
     ArgumentsTest("bird_4_different_fly_1", "../examples/inference/bird_4_different.lp", "fly(1)", 0.102222, 0.11),
+    ArgumentsTest("bird_10_fly_1","../examples/inference/bird_10.lp", "fly(1)", 0.126953, 0.5),
+    ArgumentsTest("burglary_qr", "../examples/inference/burglary.lp", "qr", 0.5, 0.5),
+    ArgumentsTest("certain_fact_a_1", "../examples/inference/certain_fact_2.lp", "a(1)", 1.0, 1.0),
+    ArgumentsTest("clique_in_1", "../examples/inference/clique.lp", "in(1)", 0.4666666666666667, 0.9333333333333333),
+    ArgumentsTest("credal_facts_sleep_bill", "../examples/inference/credal_facts.lp", "sleep(bill)", 0.0, 0.0),
     ArgumentsTest("disjunction", "../examples/inference/disjunction.lp", "f", 0.6, 0.8),
-    ArgumentsTest("graph_coloring_qr", "../examples/inference/graph_coloring.lp", "qr", 0.03, 1),
+    ArgumentsTest("evidence_certain_a", "../examples/inference/evidence_certain.lp", "a", 1.0, 1.0),
+    ArgumentsTest("graph_coloring_qr", "../examples/inference/graph_coloring.lp", "qr", 0.03, 1.0),
+    ArgumentsTest("monty_hall", "../examples/inference/monty_hall.lp", "win_switch", 0.6666666666666667, 0.6666666666666667),
+    ArgumentsTest("multiple_ad", "../examples/inference/multiple_ad.lp", "qr", 0.0975, 0.5775),
     ArgumentsTest("path_path_1_4", "../examples/inference/path.lp", "path(1,4)", 0.266816, 0.266816),
     ArgumentsTest("shop_qr", "../examples/inference/shop.lp", "qr", 0.096, 0.3),
     ArgumentsTest("sick_sick", "../examples/inference/sick.lp", "sick", 0.2, 0.2384),
     ArgumentsTest("smoke_qry", "../examples/inference/smoke.lp", "qry", 0, 0.09),
-    ArgumentsTest("smoke_3", "../examples/inference/smoke_3.lp", "qry", 0.3, 0.3),
+    #ArgumentsTest("smoke_sm_smokes_1", "../examples/inference/smoke_sm.lp", "smokes(1)", )
+    #ArgumentsTest("smoke_qry", "../examples/inference/smoke_2.lp", "qr", )
+    ArgumentsTest("smoke_3_qry", "../examples/inference/smoke_3.lp", "qry", 0.3, 0.3),
+    ArgumentsTest("transmission_transmit_a_e", "../examples/inference/transmission.lp" , "transmit(a,e)", 0.15839999999999999, 0.6537600000000001),
     ArgumentsTest("viral_marketing_5_buy_5", "../examples/inference/viral_marketing_5.lp", "buy(5)", 0.2734, 0.29)
 ])
 def test_approximate_inference(parameters : ArgumentsTest):
--- test/test_exact_inference.py
+++ test/test_exact_inference.py
@@ -6,20 +6,30 @@ from pastasolver.pasta_solver import Pasta
 
 
 @pytest.mark.parametrize("filename,query,evidence,test_name,expected_lp,expected_up,normalize",[
-    ("../examples/inference/bird_2_2.lp","fly_1", "", "bird_2_2_fly_1",0.6, 0.7, False),
+    ("../examples/inference/alarm.lp", "calls(mary)", "", "alarm_calls_mary", 0.196, 0.196, False),
+    ("../examples/inference/barber.lp", "person(j)", "", "barber_person_j", 1.0, 1.0, False),
+    ("../examples/inference/bird_2_2.lp","fly_1", "", "bird_2_2_fly_1", 0.6, 0.7, False),
     ("../examples/inference/bird_4.lp","fly(1)", "", "bird_4_fly_1", 0.25, 0.5, False),
     ("../examples/inference/bird_4_different.lp", "fly(1)", "", "bird_4_different_fly_1", 0.102222, 0.11, False),
     ("../examples/inference/bird_10.lp","fly(1)", "", "bird_10_fly_1", 0.126953, 0.5, False),
-    ("../examples/inference/path.lp","path(1,4)", "", "path_path_1_4", 0.266816, 0.266816, False),
-    ("../examples/inference/viral_marketing_5.lp", "buy(5)", "", "viral_marketing_5_buy_5", 0.2734, 0.29, False),
-    ("../examples/inference/disjunction.lp", "f", "", "disjunction", 0.6, 0.8, False),
-    ("../examples/inference/multiple_ad.lp", "qr", "", "multiple_ad", 0.0975, 0.5775, False),
-    ("../examples/inference/smoke_3.lp", "qry", "", "smoke_3", 0.3, 0.3, False),
+    ("../examples/inference/burglary.lp", "qr", "", "burglary_qr", 0.5, 0.5, False),
+    ("../examples/inference/certain_fact_2.lp", "a(1)", "", "certain_fact_a_1", 1.0, 1.0, False ),
     ("../examples/inference/clique.lp", "in(1)", "", "clique_in_1", 0.4666666666666667, 0.9333333333333333, True),
+    ("../examples/inference/credal_facts.lp", "sleep(bill)", "", "credal_facts_sleep_bill", 0.0, 0.0, False),
+    ("../examples/inference/disjunction.lp", "f", "", "disjunction", 0.6, 0.8, False),
+    ("../examples/inference/evidence_certain.lp", "a", "", "evidence_certain_a", 1.0, 1.0, False), 
     ("../examples/inference/graph_coloring.lp", "qr", "", "graph_coloring_qr", 0.03, 1.0, False),
+    ("../examples/inference/monty_hall.lp", "win_switch", "", "monty_hall", 0.6666666666666667, 0.6666666666666667, False), 
+    ("../examples/inference/multiple_ad.lp", "qr", "", "multiple_ad", 0.0975, 0.5775, False),
+    ("../examples/inference/path.lp","path(1,4)", "", "path_path_1_4", 0.266816, 0.266816, False),
     ("../examples/inference/shop.lp", "qr", "", "shop_qr", 0.096, 0.3, False),
     ("../examples/inference/sick.lp", "sick", "", "sick_sick", 0.2, 0.2384, False),
-    ("../examples/inference/smoke.lp", "qry", "", "smoke_qry", 0, 0.09, False)
+    ("../examples/inference/smoke.lp", "qry", "", "smoke_qry", 0, 0.09, False),
+    #("../examples/inference/smoke_sm.lp", "smokes(1)", "", "smoke_sm_smokes_1",  ),
+    #("../examples/inference/smoke_2.lp", "qr", "", "smoke_qry", ),
+    ("../examples/inference/smoke_3.lp", "qry", "", "smoke_3_qry", 0.3, 0.3, False),
+    ("../examples/inference/transmission.lp" , "transmit(a,e)", "", "transmission_transmit_a_e", 0.15839999999999999, 0.6537600000000001, False),
+    ("../examples/inference/viral_marketing_5.lp", "buy(5)", "", "viral_marketing_5_buy_5", 0.2734, 0.29, False)
 ])
 def test_exact_inference( 
     filename : str,
