01d13ed9f3dc4bf5f35c913a2a91610586f6ec33
Edoardo Girotto <edoardo@Air-di-Edoardo.homenet.telecomitalia.it>
2025-03-10 17:01:29 +0100

    Refactoring e aggiunta test per abduction, approximate_inference, exact_inference, lifted

--- test/test_abduction.py
+++ test/test_abduction.py
@@ -1,18 +1,14 @@
-# import utils_for_tests
+import pytest
 from .utils_for_tests import almost_equal, check_if_lists_equal
-
 from pastasolver.pasta_solver import Pasta
 
-
-def wrap_test_abduction(
-                        filename: str,
+def wrap_test_abduction(filename: str,
                         query: str,
                         evidence: str,
                         test_name: str,
                         expected_lp: float,
                         expected_up: float,
                         expected_abd: 'list[list[str]]'):
-
     pasta_solver = Pasta(filename, query, evidence)
     lp, up, abd = pasta_solver.abduction()
 
@@ -20,150 +16,25 @@ def wrap_test_abduction(
     assert almost_equal(up, expected_up), test_name + ": wrong upper probability"
     assert check_if_lists_equal(abd, expected_abd), f"{test_name}: wrong abduction. Found {abd} expected {expected_abd}"
 
-def test_bird_4_abd_prob():
-    wrap_test_abduction(
-        "../examples/abduction/bird_4_abd_prob.lp",
-        "fly(1)",
-        "",
-        "bird_4_abd_prob",
-        0.5,
-        0.5,
-        [['fa(1)']]
-    )
-
-def test_ex_1_det():
-    wrap_test_abduction(
-        "../examples/abduction/ex_1_det.lp",
-        "query",
-        "",
-        "ex_1_det",
-        1,
-        1,
-        [['a', 'b']]
-    )
-
-def test_ex_1_prob():
-    wrap_test_abduction(
-        "../examples/abduction/ex_1_prob.lp",
-        "query",
-        "",
-        "ex_1_prob",
-        0.25,
-        0.25,
-        [['a', 'b']]
-    )
-
-def test_ex_2_det():
-    wrap_test_abduction(
-        "../examples/abduction/ex_2_det.lp",
-        "query",
-        "",
-        "ex_2_det",
-        1,
-        1,
-        [['b'],['a']]
-    )
-
-def test_ex_2_prob():
-    wrap_test_abduction(
-        "../examples/abduction/ex_2_prob.lp",
-        "query", "", "ex_2_prob",
-        0.75,
-        0.75,
-        [['a', 'b']]
-    )
-
-def test_ex_3_det():
-    wrap_test_abduction(
-        "../examples/abduction/ex_3_det.lp",
-        "query",
-        "",
-        "ex_3_det",
-        1,
-        1,
-        [   ['a'],
-            ['c', 'b']
-        ]
-    )
-
-def test_ex_3_prob():
-    wrap_test_abduction(
-        "../examples/abduction/ex_3_prob.lp",
-        "query",
-        "",
-        "ex_3_prob",
-        0.58,
-        0.58,
-        [['a', 'b', 'c']]
-    )
-
-def test_ex_4_det():
-    wrap_test_abduction(
-        "../examples/abduction/ex_4_det.lp",
-        "query",
-        "",
-        "ex_4_det",
-        1,
-        1,
-        [   ['a(1)'],
-            ['c', 'b']
-        ]
-    )
-
-# this should rise an inconsistency error
-# def test_ex_4_prob(self):
-#     self.wrap_test_abduction("../examples/abduction/ex_4_prob.lp",
-#                              "query", "", "ex_4_prob", 0.648, 0.648, [['c', 'e']])
-
-def test_ex_5_det():
-    wrap_test_abduction(
-        "../examples/abduction/ex_5_det.lp",
-        "qr",
-        "",
-        "ex_5_det",
-        1,
-        1,
-        [   ['c', 'b', 'a'],
-            ['e', 'd', 'b', 'a',]
-        ]
-    )
-
-def test_ex_6_prob():
-    wrap_test_abduction(
-        "../examples/abduction/ex_6_prob.lp",
-        "qry",
-        "",
-        "ex_6_prob",
-        1.0,
-        1.0,
-        [['a(1)']]
-    )
-
-def test_kn_4_prob():
-    wrap_test_abduction(
-        "../examples/abduction/kn_4_prob.lp",
-        "path(1,4)",
-        "",
-        "kn_4_prob",
-        0.734375,
-        0.734375,
-        [['edge(1,2)','edge(1,3)','edge(1,4)','edge(2,3)','edge(2,4)','edge(3,4)']]
-    )
-
-def test_smokes_det():
-    wrap_test_abduction(
-        "../examples/abduction/smokes_det.lp",
-        "smokes(c)",
-        "",
-        "smokes_det",
-        1,
-        1,
-        [   ['e(b,c)'],
-            ['e(e,c)', 'e(d,e)']
-        ]
-    )
-
-# this should rise an inconsistency error
-# def test_smokes_prob(self):
-#     self.wrap_test_abduction("../examples/abduction/smokes_prob.lp", "smokes(c)", "", "smokes_prob",
-#                              0.125, 0.425, [['not e(a,b)', 'e(b,c)', 'not e(a,d)', 'e(d,e)', 'e(e,c)']])
+@pytest.mark.parametrize(
+    "filename,query,evidence,test_name,expected_lp,expected_up,expected_abd",
+    [
+        ("../examples/abduction/bird_4_abd_prob.lp", "fly(1)", "", "bird_4_abd_prob", 0.5, 0.5, [['fa(1)']]),
+        ("../examples/abduction/ex_1_det.lp", "query", "", "ex_1_det", 1, 1, [['a', 'b']]),
+        ("../examples/abduction/ex_1_prob.lp", "query", "", "ex_1_prob", 0.25, 0.25, [['a', 'b']]),
+        ("../examples/abduction/ex_2_det.lp", "query", "", "ex_2_det", 1, 1, [['b'], ['a']]),
+        ("../examples/abduction/ex_2_prob.lp", "query", "", "ex_2_prob", 0.75, 0.75, [['a', 'b']]),
+        ("../examples/abduction/ex_3_det.lp", "query", "", "ex_3_det", 1, 1, [['a'], ['c', 'b']]),
+        ("../examples/abduction/ex_3_prob.lp", "query", "", "ex_3_prob", 0.58, 0.58, [['a', 'b', 'c']]),
+        ("../examples/abduction/ex_4_det.lp", "query", "", "ex_4_det", 1, 1, [['a(1)'], ['c', 'b']]),
+        # Test ex_4_prob omesso: errore di inconsistenza
+        ("../examples/abduction/ex_5_det.lp", "qr", "", "ex_5_det", 1, 1, [['c', 'b', 'a'], ['e', 'd', 'b', 'a']]),
+        ("../examples/abduction/ex_6_prob.lp", "qry", "", "ex_6_prob", 1.0, 1.0, [['a(1)']]),
+        ("../examples/abduction/kn_4_prob.lp", "path(1,4)", "", "kn_4_prob", 0.734375, 0.734375,
+         [['edge(1,2)', 'edge(1,3)', 'edge(1,4)', 'edge(2,3)', 'edge(2,4)', 'edge(3,4)']]),
+        ("../examples/abduction/smokes_det.lp", "smokes(c)", "", "smokes_det", 1, 1,
+         [['e(b,c)'], ['e(e,c)', 'e(d,e)']])
+    ]
+)
+def test_abduction(filename, query, evidence, test_name, expected_lp, expected_up, expected_abd):
+    wrap_test_abduction(filename, query, evidence, test_name, expected_lp, expected_up, expected_abd)
--- test/test_lifted.py
+++ test/test_lifted.py
@@ -1,79 +1,50 @@
+import pytest
 from .utils_for_tests import almost_equal
-
 import pastasolver.lifted.lifted as lft
 
-
-# (c(X,Y) | a(X), b(X,Y))[lb,ub] multiple
-# def test_smoke_2_qr_exit(self):
-#     with pytest.raises(SystemExit):
-#         self.wrap_test_exact_inference("../examples/inference/smoke_2.lp", "qr", "", "smoke_2_qr", 0.055408970976253295, 0.13398746701846967)
-
-def test_cxy_ax_bxy_multiple_bi_1_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_1_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 3], lower=40)
-    assert almost_equal(lp, 0.0576), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_1_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_1_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 3], lower=0, upper=70)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.10240), test_name + ": wrong upper probability"
-
-def test_cxy_ax_bxy_multiple_bi_2_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_2_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 2, 2], lower=40)
-    assert almost_equal(lp, 0.071424), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_2_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_2_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 2, 2], lower=0, upper=70)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.088576), test_name + ": wrong upper probability"
-
-
-def test_cxy_ax_bxy_multiple_bi_3_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_3_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 1, 2, 2, 1], lower=40)
-    assert almost_equal(lp, 0.059996160), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_3_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_3_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 1, 2, 2, 1], lower=0, upper=70)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.10000384), test_name + ": wrong upper probability"
-
-
-def test_cxy_ax_bxy_multiple_bi_4_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_4_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 3, 2], lower=40)
-    assert almost_equal(lp, 0.0428544), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_4_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_4_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 3, 2], lower=0, upper=70)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.1171456), test_name + ": wrong upper probability"
-
-
-def test_cxy_ax_bxy_multiple_bi_5_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_5_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 1, 3, 2, 1], lower=40)
-    assert almost_equal(lp, 0.035997696), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_5_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_5_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1, 1, 1, 3, 2, 1], lower=0, upper=70)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.1240023), test_name + ": wrong upper probability"
-
-
-def test_cxy_ax_bxy_multiple_bi_6_lower():
-    test_name = "test_cxy_ax_bxy_multiple_bi_6_lower"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1,1,1,1,1,3,2,2,1,1], lower=10)
-    assert almost_equal(lp, 0.02249712), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.16), test_name + ": wrong upper probability"
-def test_cxy_ax_bxy_multiple_bi_6_upper():
-    test_name = "test_cxy_ax_bxy_multiple_bi_6_upper"
-    lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, [1,1,1,1,1,3,2,2,1,1], lower=0, upper=80)
-    assert almost_equal(lp, 0), test_name + ": wrong lower probability"
-    assert almost_equal(up, 0.137502879), test_name + ": wrong upper probability"
+@pytest.mark.parametrize(
+    "params, lower_val, upper_val, expected_lp, expected_up, test_name",
+    [
+        # Test 1: [1, 3]
+        ([1, 3], 40, None, 0.0576, 0.16, "test_cxy_ax_bxy_multiple_bi_1_lower"),
+        ([1, 3], 0, 70, 0, 0.10240, "test_cxy_ax_bxy_multiple_bi_1_upper"),
+        
+        # Test 2: [1, 1, 2, 2]
+        ([1, 1, 2, 2], 40, None, 0.071424, 0.16, "test_cxy_ax_bxy_multiple_bi_2_lower"),
+        ([1, 1, 2, 2], 0, 70, 0, 0.088576, "test_cxy_ax_bxy_multiple_bi_2_upper"),
+        
+        # Test 3: [1, 1, 1, 2, 2, 1]
+        ([1, 1, 1, 2, 2, 1], 40, None, 0.059996160, 0.16, "test_cxy_ax_bxy_multiple_bi_3_lower"),
+        ([1, 1, 1, 2, 2, 1], 0, 70, 0, 0.10000384, "test_cxy_ax_bxy_multiple_bi_3_upper"),
+        
+        # Test 4: [1, 1, 3, 2]
+        ([1, 1, 3, 2], 40, None, 0.0428544, 0.16, "test_cxy_ax_bxy_multiple_bi_4_lower"),
+        ([1, 1, 3, 2], 0, 70, 0, 0.1171456, "test_cxy_ax_bxy_multiple_bi_4_upper"),
+        
+        # Test 5: [1, 1, 1, 3, 2, 1]
+        ([1, 1, 1, 3, 2, 1], 40, None, 0.035997696, 0.16, "test_cxy_ax_bxy_multiple_bi_5_lower"),
+        ([1, 1, 1, 3, 2, 1], 0, 70, 0, 0.1240023, "test_cxy_ax_bxy_multiple_bi_5_upper"),
+        
+        # Test 6: [1, 1, 1, 1, 1, 3, 2, 2, 1, 1]
+        ([1, 1, 1, 1, 1, 3, 2, 2, 1, 1], 10, None, 0.02249712, 0.16, "test_cxy_ax_bxy_multiple_bi_6_lower"),
+        ([1, 1, 1, 1, 1, 3, 2, 2, 1, 1], 0, 80, 0, 0.137502879, "test_cxy_ax_bxy_multiple_bi_6_upper"),
+    ]
+)
+def test_cxy_ax_bxy_multiple_bi(params, lower_val, upper_val, expected_lp, expected_up, test_name):
+    """
+    Test parametrizzati per la funzione cxy_ax_bxy_multiple_bi.
+    
+    Se 'upper_val' è None, il test viene eseguito passando solo il parametro 'lower';
+    altrimenti vengono passati sia 'lower' che 'upper'.
+    """
+    if upper_val is None:
+        lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, params, lower=lower_val)
+    else:
+        lp, up, _, _ = lft.cxy_ax_bxy_multiple_bi(0.4, params, lower=lower_val, upper=upper_val)
+    
+    assert almost_equal(lp, expected_lp), (
+        f"{test_name}: probabilità inferiore errata - atteso: {expected_lp}, ottenuto: {lp}"
+    )
+    assert almost_equal(up, expected_up), (
+        f"{test_name}: probabilità superiore errata - atteso: {expected_up}, ottenuto: {up}"
+    )
